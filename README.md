a3e91fadc1e81fc4114595e4dedd1813a6bd49fb
cheng <834037683@qq.com>
2017-03-23 14:49:34 +0800

    gulp

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# gulp
-js合并压缩与雪碧图合并
+#该文件作用
+    compressJS：实现合并压缩重命名js文件
+    compressCss：实现合并压缩重命名css文件
+    compressPic：实现压缩jpg,png图片
+    sprites：实现合并背景图片
+
+
+
+
+#第一步
+    下载包依赖   npm install
+
+#第二步
+    执行  gulp
+
+#最终
+    生成一个dist文件夹,生成处理后的文件
+
+#package.json 包依赖
+#unpressdist 这文件夹是未处理的图片，css，js，演示查看效果的，具体可以根据需求修改gulpfile.js文件
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,65 @@
-var gulp=require('gulp');
-var spritesmith=require('gulp.spritesmith');
-var concat=require("gulp-concat");
-var uglify=require('gulp-uglify');
-var pump=require('pump');
+/**
+ * Created by Administrator on 2017/3/23.
+ */
+var gulp=require("gulp");
+var concat=require("gulp-concat");   //合并代码
+var uglify=require("gulp-uglify");   //压缩js代码
+var rename=require("gulp-rename");   //文件重命名
+var minifyCss=require("gulp-minify-css");  //压缩css
+var imagemin=require("gulp-imagemin");    //压缩图片
+var spritesmith=require("gulp.spritesmith");  //合并背景图片
+var jshint=require("gulp-jshint");      //检测js代码
 
 
-/*合并图片*/
-gulp.task('sprite',function(){
-   var spriteData=gulp.src('images/*.png').pipe(spritesmith({
-       imgName:'sprite.png',
-       cssName:'sprite.css'
-   }));
-    return spriteData.pipe(gulp.dest('path/to/output'));
-});
 
-/*压缩合并js*/
-gulp.task("concat",function(){
-   return gulp.src('js/*.js')
+var name="app";   //保存css,js文件的名字
+
+
+//合并压缩重命名js
+gulp.task("compressJS",function(){
+    return gulp.src("unpressdist/js/*.js")
+            .pipe(concat(name+".js"))
+            .pipe(gulp.dest("dist/js"))
             .pipe(uglify())
-            .pipe(concat('all.min.js'))
-            .pipe(gulp.dest('./dist/'));
+            .pipe(jshint())
+            .pipe(jshint.reporter())
+            .pipe(rename(name+".min.js"))
+            .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('default',['concat','sprite']);
+//合并压缩重命名css
+gulp.task("compressCss",function(){
+    return gulp.src("unpressdist/css/*.css")
+            .pipe(concat(name+".css"))
+            .pipe(gulp.dest("dist/css"))
+            .pipe(minifyCss())
+            .pipe(rename(name+".min.css"))
+            .pipe(gulp.dest("dist/css"));
+});
+
+//压缩jpg,png图片
+gulp.task("compressPic",function(){
+    return gulp.src("unpressdist/pic/*.{jpg,png}")
+            .pipe(imagemin())
+            .pipe(gulp.dest("dist/pic"));
+});
+
+//合并雪碧图
+gulp.task("sprites",function(){
+    return gulp.src("unpressdist/sprite/*.{jpg,png}")
+            .pipe(spritesmith({
+                imgName:'sprite.png',
+                cssName:'sprite.css'
+            }))
+            .pipe(gulp.dest("dist/sprite"));
+});
+
+gulp.task('default',['compressJS','compressCss','compressPic','sprites']);   //执行
+
+/*
+ 如果需要实时监控取消该注释
+gulp.watch('js/!*.js',['compressJS','compressCss','compressPic','sprites']);
+*
+*
+*  */
 
